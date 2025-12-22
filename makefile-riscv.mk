26f1daa2357ffd4f64a5beffad344c8fc597b351
liyuxuan <liyuxuan3003@outlook.com>
2025-12-22 13:43:03 +0800

    将ld整合到模板中，增加了dump的支持

--- makefile-riscv.mk
+++ makefile-riscv.mk
@@ -7,26 +7,14 @@ PROJECT?=ProjectRiscv
 # Build directory
 BUILD_DIR?=build
 
-# Compiler
-CC_BIN:=riscv-none-elf-gcc
-
-# Flags for compile
-CC_FLAGS?=
-
-# Flags for link
-LD_FLAGS?=
-
-# Tool objcopy
-OBJCOPY?=riscv-none-elf-objcopy
-
-# Flags for objcopy
-OBJCOPY_FLAGS?=-O binary
-
 # Source files for C (.c)
 SRCS_C?=$(wildcard *.c)
 
 # Source files for ASM (.s)
 SRCS_S?=$(wildcard *.s)
+
+# Source files for LD (.ld)
+SRCS_LD?=$(wildcard *.ld)
 
 # Object files (.o)
 OBJS?=$(addprefix ${BUILD_DIR}/,$(SRCS_C:.c=.o) $(SRCS_S:.s=.o))
@@ -37,8 +25,32 @@ DEPS?=$(OBJS:.o=.d)
 # Program
 PROGRAM?=${BUILD_DIR}/${PROJECT}
 
-# Bin
+# Bin (objcopy of program)
 BINFILE?=${BUILD_DIR}/${PROJECT}.bin
+
+# Dump (objdump of program, disassembly)
+DMPFILE?=${BUILD_DIR}/${PROJECT}.dump
+
+# Compiler
+CC_BIN:=riscv-none-elf-gcc
+
+# Tool objcopy
+OBJCOPY?=riscv-none-elf-objcopy
+
+# Tool objdump
+OBJDUMP?=riscv-none-elf-objdump
+
+# Flags for compile
+CC_FLAGS?=
+
+# Flags for link
+LD_FLAGS?=
+
+# Flags for objcopy
+OBJCOPY_FLAGS?=-O binary
+
+# Flags for objdump
+OBJDUMP_FLAGS?=-D
 
 # --------------------------------
 # Notification at the end of the task
@@ -49,10 +61,14 @@ endef
 
 # --------------------------------
 # Declare phony tasks
-.PHONY: default clean
+.PHONY: default dump clean
 
 # Generate bin file
 default: ${BUILD_DIR} ${BINFILE}
+	${NOTIFY_DONE}
+
+# Generate dump file
+dump: ${BUILD_DIR} ${DMPFILE}
 	${NOTIFY_DONE}
 
 # Clean build directory
@@ -71,18 +87,23 @@ ${BINFILE}: ${PROGRAM}
 	${OBJCOPY} ${OBJCOPY_FLAGS}	$< $@
 	${NOTIFY_DONE}
 
-# Link object files to program
-${PROGRAM}: ${OBJS}
-	${CC_BIN} ${LD_FLAGS} -o $@ $^
+# Generate dump file
+${DMPFILE}: ${PROGRAM}
+	${OBJDUMP} ${OBJDUMP_FLAGS} -S $< > $@
 	${NOTIFY_DONE}
 
-# Compile c source file to object file 
-${BUILD_DIR}/%.o: %.c | ${BUILD_DIR}
-	${CC_BIN} ${CC_FLAGS} -o $@ -c $<
+# Link object files to program
+${PROGRAM}: ${SRCS_LD} ${OBJS}
+	${CC_BIN} ${LD_FLAGS} -T ${SRCS_LD} -nostartfiles -o $@ ${OBJS}
 	${NOTIFY_DONE}
 
 # Compile asm source file to object file 
 ${BUILD_DIR}/%.o: %.s | ${BUILD_DIR}
+	${CC_BIN} ${CC_FLAGS} -o $@ -c $<
+	${NOTIFY_DONE}
+
+# Compile c source file to object file 
+${BUILD_DIR}/%.o: %.c | ${BUILD_DIR}
 	${CC_BIN} ${CC_FLAGS} -o $@ -c $<
 	${NOTIFY_DONE}
 
